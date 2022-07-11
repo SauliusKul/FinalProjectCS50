b2622fdd35ef6e71581b031527247dc02b3032ba
root <root@Saulekokompakas.localdomain>
2022-07-11 21:16:42 +0100

    added flexbox scaffold and an additional page which is currently not working

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
+from cs50 import SQL
 from flask import Flask, render_template
+from flask_session import Session
 
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
+app.config["SESSION_TYPE"] = "filesystem"
+app.config["SESSION_PERMANENT"] = False
+Session(app)
 
 @app.route("/")
 def hello_world():
     a = 1 + 3
     hi = "hiya"
     return render_template("index.html")
+
+@app.route("/leaderboard")
+def leaderboard():
+    return render_template("leaderboard.html")
