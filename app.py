c57b40d47dec7b7b24d162b67d47d7894883484f
root <root@Saulekokompakas.localdomain>
2022-08-19 18:51:43 +0100

    Heroku compatibility

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import sqlite3
-import json
 from flask import Flask, render_template, url_for, request, flash, redirect, session, get_flashed_messages
 from flask_session import Session
 from flask_socketio import SocketIO, send, emit
